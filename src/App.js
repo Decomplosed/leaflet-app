644b9a155c2b0a99d3a7720e17300a8a2e05bbd4
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-10 14:47:30 +0200

    defined state and position variable

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css'
 
 class App extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng]
+
     return (
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer
